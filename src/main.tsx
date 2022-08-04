0be81a048b7764bc5ccd33d55ad8b75e0036b62f
Ryan Shea <shayded@shayded.com>
2022-08-04 18:15:35 -0500

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import './index.css';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const rootElement = document.getElementById('root')!;
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+);
