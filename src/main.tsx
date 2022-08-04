28393288e99db8555276e8795c7df3a9b86a46cc
Ryan Shea <shayded@shayded.com>
2022-08-04 18:22:02 -0500

    set default theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import './index.css';
-
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { THEME } from './theme';
 
 const rootElement = document.getElementById('root')!;
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={THEME}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
